0a198be2b6e3d243fccb3bb175836471f5b33fe9
Mina Shah <minashah814@gmail.com>
2025-01-09 02:26:22 +0500

    made style changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,22 +21,23 @@ const About = () => {
         <h1 className="text-2xl text-slate-500 mb-6 text-center sm:text-left">
           Tech Stack
         </h1>
-        <div className="flex flex-wrap justify-center items-center gap-6">
+        <div className="flex flex-wrap justify-center items-center gap-5">
           {[
-            { src: "images/html.png", alt: "HTML Logo", name: "HTML" },
-            { src: "images/css.png", alt: "CSS Logo", name: "CSS" },
-            { src: "images/js.png", alt: "JavaScript Logo", name: "JavaScript" },
-            { src: "images/tailwind.png", alt: "Tailwind CSS Logo", name: "Tailwind CSS" },
-            { src: "images/vite.png", alt: "Vite Logo", name: "Vite" },
-            { src: "images/webpack.png", alt: "Webpack Logo", name: "Webpack" },
-            { src: "images/react.png", alt: "React Logo", name: "React" },
-            { src: "images/git.png", alt: "Git Logo", name: "Git" },
-            { src: "images/github.jpg", alt: "GitHub Logo", name: "GitHub" },
+            { src: "images/logos/html.png", alt: "HTML Logo", name: "HTML" },
+            { src: "images/logos/css.png", alt: "CSS Logo", name: "CSS" },
+            { src: "images/logos/js.png", alt: "JavaScript Logo", name: "JavaScript" },
+            { src: "images/logos/tailwind.png", alt: "Tailwind CSS Logo", name: "Tailwind CSS" },
+            { src: "images/logos/vite.png", alt: "Vite Logo", name: "Vite" },
+            { src: "images/logos/webpack.png", alt: "Webpack Logo", name: "Webpack" },
+            { src: "images/logos/react.png", alt: "React Logo", name: "React" },
+            { src: "images/logos/react-router-dom.png", alt: "React Router Logo", name: "React-Router-Dom"},
+            { src: "images/logos/git.png", alt: "Git Logo", name: "Git" },
+            { src: "images/logos/github.jpg", alt: "GitHub Logo", name: "GitHub" },
           ].map((tech) => (
             <div key={tech.name} className="flex flex-col items-center">
               <img
                 src={tech.src}
-                className="w-12 h-12 animate-pulse hover:animate-none"
+                className="w-12 h-11 animate-pulse hover:animate-none"
                 alt={tech.alt}
               />
               <p className="mt-2 text-sm text-gray-400">{tech.name}</p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Projects from "./Projects";
 import About from "./About";
-import Contact from "./Contact"
+
 const Home = () => {
   const navigate = useNavigate();
-
+  
   return (
      <>
   <div className="flex flex-col items-center justify-center text-center p-5 animate-slide-in-up">     
@@ -15,16 +16,23 @@ const Home = () => {
         Hi, I am Mina Shah, a frontend developer crafting seamless and engaging web
         experiences.
       </p>
-      <button
-        className="bg-yellow-500 text-neutral-900 font-semibold px-6 py-2 rounded-lg shadow-lg hover:bg-yellow-600 transition duration-300"
-        onClick={() => navigate("/projects")}
-      >
-        View My Projects
-      </button>
-    </div>
+      <div className="flex space-x-4 mb-6">
+          <button
+            className="text-white border border-white px-6 py-2 rounded-lg shadow-lg hover:bg-neutral-800 transition duration-300"
+            onClick={() => navigate("/projects")}
+          >
+            View My Projects
+          </button>
+          <button
+            onClick={() => navigate("/contact")}
+            className="px-6 py-3 bg-yellow-500 text-black text-lg font-semibold rounded-lg shadow-md hover:bg-yellow-600"
+          >
+            Contact Me
+          </button>
+         </div>
+         </div>
     <About/>
     <Projects/>
-    <Contact/>
     </>
   );
 };
