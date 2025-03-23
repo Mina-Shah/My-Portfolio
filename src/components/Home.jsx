4d9e5e075a0b85324269cd05b3f6e06d3a063449
Mina Shah <shahmina814@example.com>
2025-03-23 06:37:11 +0500

    style changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,10 @@ const images = [
   "images/p1.PNG",
   "images/p4.PNG",
   "images/p6.PNG",
-   "images/p2.PNG"
+  "images/p2.PNG",
 ];
 
 const loopImages = [...images, ...images];
-
 
 const Home = () => {
   const navigate = useNavigate();
@@ -20,22 +19,23 @@ const Home = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center text-center p-5 animate-slide-in-up w-full max-w-6xl mx-auto">
-      <div className="flex flex-col items-center">
-              <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mt-20 md:mt-28 lg:mt-32 mb-4">
-                Frontend Developer.
-              </h1>
-              <p className="text-sm tracking-wide md:text-lg lg:text-xl px-4 mb-2">
-              <span>Hi! I am Mina Asif, a frontend developer.</span> <br />
-              <span>Crafting seamless and engaging web experiences.</span>
-              </p>
-            </div>
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mt-20 md:mt-28 lg:mt-32 mb-4">
+            Frontend Developer.
+          </h1>
+          <p className="text-sm tracking-wide md:text-lg lg:text-xl px-4 mb-2">
+            <span>Hi! I am Mina Asif, a frontend developer.</span> <br />
+            <span>Crafting seamless and engaging web experiences.</span>
+          </p>
+        </div>
 
-            <div className="flex flex-col sm:flex-row sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-6 w-ful justify-center mb-6">
+        <div className="flex flex-col sm:flex-row sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-6 w-ful justify-center mb-6">
           <a
-            href="images/Resume MIna Shah.jpg"
+            href="/Mina_Asif_CV.pdf"
+            download="Mina_Asif_CV.pdf"
             className="text-white border border-white text-lg px-6 py-3 rounded-3xl shadow-lg hover:bg-neutral-800 transition duration-300 text-center w-full sm:w-auto"
           >
-            Resume
+            My CV
           </a>
           <button
             onClick={() => navigate("/contact")}
@@ -46,28 +46,28 @@ const Home = () => {
         </div>
 
         <div className="relative overflow-hidden w-full max-w-3xl mx-auto mt-20">
-      {/* Left Shadow */}
-      <div className="absolute top-0 left-0 h-full w-32 bg-gradient-to-r from-black/100 via-black/60 to-transparent z-10 pointer-events-none" />
+          {/* Left Shadow */}
+          <div className="absolute top-0 left-0 h-full w-32 bg-gradient-to-r from-black/100 via-black/60 to-transparent z-10 pointer-events-none" />
 
-      {/* Right Shadow */}
-      <div className="absolute top-0 right-0 h-full w-32 bg-gradient-to-l from-black/100 via-black/60 to-transparent z-10 pointer-events-none" />
+          {/* Right Shadow */}
+          <div className="absolute top-0 right-0 h-full w-32 bg-gradient-to-l from-black/100 via-black/60 to-transparent z-10 pointer-events-none" />
 
-      {/* Image Slider */}
-      <motion.div
-        className="flex gap-5 w-[200%]"
-        animate={{ x: ["0%", "-100%"] }}
-        transition={{ repeat: Infinity, duration: 15, ease: "linear" }}
-      >
-        {loopImages.map((src, index) => (
-          <img
-            key={index}
-            src={src}
-            alt={`Mockup ${index + 1}`}
-            className="rounded-lg w-auto h-52 object-cover shadow-2xl"
-          />
-        ))}
-      </motion.div>
-    </div>
+          {/* Image Slider */}
+          <motion.div
+            className="flex gap-5 w-[200%]"
+            animate={{ x: ["0%", "-100%"] }}
+            transition={{ repeat: Infinity, duration: 15, ease: "linear" }}
+          >
+            {loopImages.map((src, index) => (
+              <img
+                key={index}
+                src={src}
+                alt={`Mockup ${index + 1}`}
+                className="rounded-lg w-auto h-52 object-cover shadow-2xl"
+              />
+            ))}
+          </motion.div>
+        </div>
       </div>
 
       <About />
